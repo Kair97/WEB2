4bc3922445f40b4577f0c1109f6f0c1e9ba2590f
Your Name <you@example.com>
2025-12-11 19:42:17 +0500

    upgraded try1/app.js added 4,5,6,7 js files

--- try1/app.js
+++ try1/app.js
@@ -1,11 +1,28 @@
 var msg = 'Hello World'
 console.log(msg)
 
-console.log(__dirname)
-setInterval(() => {
-    console.log('hi'), 5
-})
+// console.log(__dirname)
+// setInterval(() => {
+//     console.log('hi'), 5
+// })
 
-const sayHi = (name) =>{
-    consloe.log('Hello there ${name}');
+const count = 12
+if (count < 10){
+    console.log("This is less than 10")
+}else {
+    console.log("This num is greater than 10")
+
 }
+
+// const sayHi = (name) =>{
+//     consloe.log(`Hello there ${name}`);
+// }
+
+
+const names = require('./04-names')
+const sayHi = require('./5-utils')
+const data = require('./6-alternative-flavor')
+require ('./7-mind-grenade')
+// sayHi('susan')
+// sayHi(names.john)
+// sayHi(names.andrey)

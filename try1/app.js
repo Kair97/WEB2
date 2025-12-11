1cb0b5905f53cf788826ddfb0d02cda2ffbcc3c0
Your Name <you@example.com>
2025-12-11 19:21:07 +0500

    updated try1

--- try1/app.js
+++ try1/app.js
@@ -1,2 +1,11 @@
 var msg = 'Hello World'
 console.log(msg)
+
+console.log(__dirname)
+setInterval(() => {
+    console.log('hi'), 5
+})
+
+const sayHi = (name) =>{
+    consloe.log('Hello there ${name}');
+}
